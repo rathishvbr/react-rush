4cfa2f7eb8dd809a1f8be2eebad7f13ebd97e9ed
rathish <rathishvbr@megam.io>
2018-11-18 07:58:50 +0530

    Updates

---
+++ precache-manifest.477744267de09d39f63b1e57f26655b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/live-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a4a84c66bd86c48a557f",
+    "url": "/live-app/static/js/runtime~main.a4a84c66.js"
+  },
+  {
+    "revision": "f98f65644ec1f3c75c5b",
+    "url": "/live-app/static/js/main.f98f6564.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/live-app/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "f98f65644ec1f3c75c5b",
+    "url": "/live-app/static/css/main.a7cd5c7d.chunk.css"
+  },
+  {
+    "revision": "5ce1a4764d1e8f32315a62c8eb279b23",
+    "url": "/live-app/index.html"
+  }
+];
